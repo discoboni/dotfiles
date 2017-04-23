ddca2dc805f100ccb2953c16088e9e63bf68be81
gabriel404 <gabriel404@users.noreply.github.com>
2017-04-23 18:07:22 -0300

    Script to control and display spotify music on Polybar

---
+++ polybar/spotifyControl.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import dbus
+import re
+import sys
+
+bus = dbus.SessionBus()
+
+try:
+    player = dbus.SessionBus().get_object("org.mpris.MediaPlayer2.spotify",
+            "/org/mpris/MediaPlayer2")
+
+    interface = dbus.Interface(player,
+            dbus_interface="org.mpris.MediaPlayer2.Player")
+
+    metadata = player.Get("org.mpris.MediaPlayer2.Player", "Metadata",
+            dbus_interface="org.freedesktop.DBus.Properties")
+
+    def getMeta():
+        print("[" + metadata['xesam:artist'][0] + " : " + metadata['xesam:title'] if
+            'xesam:title' in metadata else 'Unknown' + "]")
+
+    def getCommand():
+            argument = sys.argv[1]
+            if (argument == "-n"):
+                interface.Next()
+            elif (argument == "-p"):
+                interface.Previous()
+            elif (argument == "-pp"):
+                interface.PlayPause()
+
+            getMeta()
+
+    def checkArg():
+        if (len(sys.argv) > 1):
+            getCommand()
+        else:
+            getMeta()
+
+    checkArg()
+except:
+    print("SPOTIFY DOWN")
