34af6a70f81bd0f615ffc95355d63cbfa566686e
discoboni <22137947+discoboni@users.noreply.github.com>
2017-11-14 14:30:35 -0200

    Added redshift config

--- linking.sh
+++ linking.sh
@@ -6,3 +6,4 @@ ln -sf $PWD/Xresources                  $HOME/.Xresources
 ln -sf $PWD/i3/config                   $HOME/.config/i3/config
 ln -sf $PWD/polybar/config              $HOME/.config/polybar/config
 ln -sf $PWD/polybar/spotifyControl.py   $HOME/.config/polybar/spotifyControl.py   
+ln -sf $PWD/redshift.conf		$HOME/.config/redshift.conf
