298a6b3b005aad66ca32203371e4d9eb385c8153
discoboni <22137947+discoboni@users.noreply.github.com>
2017-11-13 23:07:45 -0200

    added uptime script

--- linking.sh
+++ linking.sh
@@ -2,7 +2,8 @@
 mkdir $HOME/.config/polybar
 #creates all the symbolic links
 ln -sf $PWD/init.vim                    $HOME/.config/nvim/init.vim
-ln -sf $PWD/Xresources                  $HOME/.Xresources                                                                        
+ln -sf $PWD/Xresources                  $HOME/.Xresources
 ln -sf $PWD/i3/config                   $HOME/.config/i3/config
 ln -sf $PWD/polybar/config              $HOME/.config/polybar/config
 ln -sf $PWD/polybar/spotifyControl.py   $HOME/.config/polybar/spotifyControl.py   
+ln -sf $PWD/polybar/uptime				$HOME/.config/polybar/uptime   
--- subprograms.sh
+++ subprograms.sh
@@ -1,6 +1,7 @@
 #LIST OF SUB PROGRAMS
 #meaning programs that are not as important to install right away
 
-sudo pacman -S polkit
+sudo pacman -S polkit ttf-dejavu
+pacaur -S ttf-fantasque-sans-git
 
 #Polkit: Control system-wide privileges, used to enable shutting down from i3 config
